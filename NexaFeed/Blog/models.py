f0d06a27975c13fdc28f42b43c0498875178cfc2
Daniel cato <danielcato038@gmail.com>
2025-04-13 13:39:29 +0000

    Models created

--- NexaFeed/Blog/models.py
+++ NexaFeed/Blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
+
+class Category(models.Model):
+    name =models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    
+    def __str__(self):
+       return self.name
+   
+    
+
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+    
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    author=models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)    
+    
+    def __str__(self):
+        return f"{self.author} - {self.post.content}"
+    
--- NexaFeed/Blog/views.py
+++ NexaFeed/Blog/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .models import Post,Category,Comment
 
+
+
+def index(request):
+    posts=Post.objects.all().order_by("-created_at")
+    context={
+        'posts': posts,   
+        }
+    
+    return render(request, 'Blog/index.html')
 # Create your views here.
+
+
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_at")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "category.html", context)
+
+
+
+
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog/detail.html", context)
